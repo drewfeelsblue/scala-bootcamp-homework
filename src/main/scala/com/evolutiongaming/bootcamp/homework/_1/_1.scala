fbb5785b343d5a2e06a8f051f505f3280e53ea39
Andrey Karasev <andrey.karasev@amobee.com>
2021-01-27 11:48:41 +0300

    First task: handle possible zero values

--- src/main/scala/com/evolutiongaming/bootcamp/homework/_1/_1.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/homework/_1/_1.scala
@@ -4,13 +4,11 @@ import scala.annotation.tailrec
 
 object _1 {
   def lcm(a: Int, b: Int): Int = a * b / gcd(a, b)
-  def gcd(a: Int, b: Int): Int = {
-    @tailrec
-    def gcdRec(a: Int, b: Int): Int = a - b match {
-      case 0 => a
-      case s if s > 0 => gcdRec(s, b)
-      case _ => gcdRec(b - a, a)
-    }
-    gcdRec(a, b)
+  @tailrec
+  def gcd(a: Int, b: Int): Int = a - b match {
+    case _ if a == 0 || b == 0 => throw new IllegalArgumentException("Zero value as an argument")
+    case 0 => a
+    case s if s > 0 => gcd(s, b)
+    case _ => gcd(b - a, a)
   }
 }
--- src/test/scala/com/evolutiongaming/bootcamp/homework/_1/_1Spec.scala
+++ src/test/scala/com/evolutiongaming/bootcamp/homework/_1/_1Spec.scala
@@ -2,7 +2,7 @@ package com.evolutiongaming.bootcamp.homework._1
 
 import _1._
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers._
+import org.scalatest.matchers.should.Matchers.{thrownBy, _}
 
 class _1Spec extends AnyFlatSpec {
 
@@ -16,5 +16,10 @@ class _1Spec extends AnyFlatSpec {
 
   "gcd" should "return 1" in {
     gcd(17, 5) shouldEqual 1
+  }
+
+  "gcd" should "raise an exception" in {
+    the [IllegalArgumentException] thrownBy gcd(0, 2) should have message "Zero value as an argument"
+    the [IllegalArgumentException] thrownBy gcd(2, 0) should have message "Zero value as an argument"
   }
 }
